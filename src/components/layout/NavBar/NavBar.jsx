9514062a1742ce59f9a35134b671709b60b554e8
Agustín Dominguez <131421006+AgusDomz@users.noreply.github.com>
2023-09-28 10:54:18 -0300

    Nav agregado y en proceso

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -1,11 +1,24 @@
 import "./NavBar.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBar = () => {
+function NavBar() {
   return (
-    <>
-      <h1>NavBar</h1>
-    </>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Taldo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Nosotros</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
